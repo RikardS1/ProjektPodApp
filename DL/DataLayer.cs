1974217e8d444f7c1a1b05a319d5e47138a0437b
Rikard Söderek <153732664+RikardS1@users.noreply.github.com>
2024-10-23 12:38:23 +0200

    datalayer klart

--- DL/DataLayer.cs
+++ DL/DataLayer.cs
@@ -1,13 +1,37 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.IO;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.Xml;
+using System.Xml.Serialization;
 
 namespace DL
 {
-    public class DataLayer
+    public class DataLayer<T>
     {
+        private string PlaceHolder;
+        public void XmlSer(List<T> poddar, string filepath)
+        {
+            XmlSerializer xs = new XmlSerializer(typeof(List<T>));
+            using (FileStream fs = new FileStream(filepath, FileMode.Append, FileAccess.Write))
+            {
+                xs.Serialize(fs, poddar);
+            }
+
+        }
+
+        public List<T> XmlDeSer(string filepath)
+        {
+            XmlSerializer xs = new XmlSerializer(typeof(List<T>));
+            using (StreamReader sr = new StreamReader(filepath))
+            {
+                return (List<T>)xs.Deserialize(sr);
+            }
+        }
+
 
     }
+
 }
