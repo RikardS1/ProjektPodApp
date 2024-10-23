543532b24379a7a90433bc4254f459ea4ba74bf8
Rikard Söderek <153732664+RikardS1@users.noreply.github.com>
2024-10-23 15:49:24 +0200

    lite skit

--- ProjektPodApp/Form1.cs
+++ ProjektPodApp/Form1.cs
@@ -8,11 +8,17 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
+using System.Xml;
 
 namespace ProjektPodApp
 {
     public partial class Form1 : Form
     {
+        private XmlDocument RssDoc;
+        private XmlNodeList RssItems;
+        //private DataLayer<T> XmlData = new DataLayer<T>(); //Eftersom det inte finns någon koppling mellan GUI och DL så går inte detta
+        private string filePath = "data.xml"; //data.xml finns inte för tillfället
+
         public Form1()
         {
             InitializeComponent();
@@ -21,6 +27,50 @@ namespace ProjektPodApp
         private void RefreshButton_Click(object sender, EventArgs e)
         {
             MessageBox.Show("DU DÄR! Vill du återställa sidan?", "Du försöker återställa sidan.", MessageBoxButtons.YesNoCancel, MessageBoxIcon.Warning);
+        }
+
+        private void ManageAddButton_Click(object sender, EventArgs e)
+        {
+            string rsslink = ManageRSSTextBox.Text;
+            string name = ManageNameTextBox.Text;
+
+            try
+            {
+                XmlDocument rssDoc = new XmlDocument();
+
+                XmlNode nameNode = rssDoc.SelectSingleNode("//channel/title");
+
+                if (nameNode == null)
+                {
+                    MessageBox.Show("Kunde inte hitta en podcast vid det namnet.", "Kunde inte hitta", MessageBoxButtons.OK, MessageBoxIcon.Error);
+                }
+
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show($"Fel vid bearbetning av RSS: {ex.Message}", "Kunde inte bearbeta RSS-strömmen", MessageBoxButtons.OK, MessageBoxIcon.Error);
+            }
+
+        }
+
+        private void ManageEditButton_Click(object sender, EventArgs e)
+        {
+            MessageBox.Show("Du har klickat på ändra-knappen", "test", MessageBoxButtons.OK, MessageBoxIcon.Error);
+        }
+
+        private void ManageRemoveButton_Click(object sender, EventArgs e)
+        {
+            MessageBox.Show("Du har klickat på ta bort-knappen", "test", MessageBoxButtons.OK, MessageBoxIcon.Error);
+        }
+
+        private void ManageRSSTextBox_TextChanged(object sender, EventArgs e)
+        {
+            string rssLink = ManageRSSTextBox.Text;
+
+            if (string.IsNullOrEmpty(rssLink))
+            {
+                MessageBox.Show("Vänligen skriv in en RSS-länk", "RSS-Länk saknas!", MessageBoxButtons.OK, MessageBoxIcon.Error);
+            }
         }
     }
 }
