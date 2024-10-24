6b813b84b853f160ae15c5e2250600325d0f8d8a
Rikard Söderek <153732664+RikardS1@users.noreply.github.com>
2024-10-24 11:29:38 +0200

    flyttade seraliseraren från datalayer till serialiseringsklassen i BL

--- BL/Serializer.cs
+++ BL/Serializer.cs
@@ -1,12 +1,40 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.IO;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.Xml.Serialization;
 
 namespace BL
 {
     class Serializer
     {
+        //Serialiserarklassen
+        public void XmlSer(List<T> poddar, string filepath)
+        {
+            XmlSerializer xs = new XmlSerializer(typeof(List<T>));
+            using (FileStream fs = new FileStream(filepath, FileMode.Append, FileAccess.Write))
+            {
+                xs.Serialize(fs, poddar);
+            }
+
+        }
+
+        //Deserialiserarklassen
+        public List<T> XmlDeSer(string filepath)
+        {
+            //Kollar om det finns en fil, om inte skapar den en ny
+            if (!File.Exists(filepath))
+            {
+                return new List<T>();
+            }
+
+            XmlSerializer xs = new XmlSerializer(typeof(List<T>));
+            using (StreamReader sr = new StreamReader(filepath))
+            {
+                return (List<T>)xs.Deserialize(sr);
+            }
+        }
     }
 }
