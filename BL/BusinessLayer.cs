9875f941b0dc683d1765b1be0da14492c518bc62
Gooten <Gootenmeister@users.noreply.github.com>
2024-10-24 11:49:01 +0200

    yeet

--- BL/BusinessLayer.cs
+++ BL/BusinessLayer.cs
@@ -6,7 +6,11 @@ using System.Threading.Tasks;
 
 namespace BL
 {
+<<<<<<< Updated upstream
     public class BusinessLayer<T>
+=======
+    public class BusinessLayer
+>>>>>>> Stashed changes
     {
         public int ID { get; set; }
 
