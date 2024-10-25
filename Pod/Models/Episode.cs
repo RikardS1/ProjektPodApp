8a0ee6af3d210f0d431c880145983554ae681f3b
Kayfah191 <kayhan.fa@hotmail.com>
2024-10-25 11:15:34 +0200

    nya ändring

---
+++ Pod/Models/Episode.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Pod.Models
+{
+    public class Episode
+    {
+        public string Name { get; set; }
+        public string Summary { get; set; }
+        public string PublishedDate { get; set; }
+
+        public Episode(string name, string summary, string publishedDate)
+        {
+
+            Name = name;
+            Summary = summary;
+            PublishedDate = publishedDate;
+        }
+    }
+}
